b40b79cf149ef377347e241a7ab192d5c9a5f40d
Magnus Dreyer <magnus.dreyer@gmail.com>
2022-12-02 13:00:38 +0100

    Clean up day 1

--- src/advent_of_code_2022/core.clj
+++ src/advent_of_code_2022/core.clj
@@ -28,10 +28,95 @@
                 (map sum-it))
         question1 (apply max input)
         question2 (->> input
-             sort
-             reverse
-             (partition 3)
-             first
-             (apply +)
-             )]
+                       sort
+                       reverse
+                       (partition 3)
+                       first
+                       (apply +)
+                       )]
     [question1 question2]))
+
+(defn- calculate-score
+  [s]
+  (let [
+        dubbel (str/split s #" ")
+        opponent (first dubbel)
+        me (last dubbel)
+        score (case me
+                "X" 1
+                "Y" 2
+                "Z" 3)
+        ]
+    (if
+      (or
+        (and (= opponent "A") (= me "X"))
+        (and (= opponent "B") (= me "Y"))
+        (and (= opponent "C") (= me "Z"))
+        )
+      (+ score 3)
+      (if
+        (or
+          (and (= opponent "A") (= me "Y"))
+          (and (= opponent "B") (= me "Z"))
+          (and (= opponent "C") (= me "X"))
+          )
+        (+ score 6)
+        score
+        )
+      )
+    )
+  )
+
+
+
+(defn calculate-score-2
+  [s]
+  (let [rpc {:rock     1
+             :paper    2
+             :scissors 3}
+        abc {"A" :rock
+             "B" :paper
+             "C" :scissors}
+        xyz {"X" 0
+             "Y" 3
+             "Z" 6}
+        dubbel (str/split s #" ")
+        opponent (first dubbel)
+        me (last dubbel)
+
+        score (xyz me)
+
+        mc (case opponent
+             "A" (case me
+                   "X" :scissors
+                   "Y" :rock
+                   "Z" :paper
+                   )
+             "B" (case me
+                   "X" :rock
+                   "Y" :paper
+                   "Z" :scissors)
+             "C" (case me
+                   "X" :paper
+                   "Y" :scissors
+                   "Z" :rock))]
+    (+ score (rpc mc))))
+
+(defn day2
+  []
+  (let [question1 (->>
+                    "resources/day2.data"
+                    slurp
+                    str/split-lines
+                    (map calculate-score)
+                    (apply +)
+                    )
+        question2 (->>
+                    "resources/day2.data"
+                    slurp
+                    str/split-lines
+                    (map calculate-score-2)
+                    (apply +))]
+
+    [question1 question2]
+    ))
