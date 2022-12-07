a34f3a7c7505fb6c2a5965e7458fb450e48f9a29
Magnus Dreyer <magnus.dreyer@gmail.com>
2022-12-07 10:44:31 +0100

    day5 done, a tad late

--- src/advent_of_code_2022/aoc_utils.clj
+++ src/advent_of_code_2022/aoc_utils.clj
@@ -1,1 +1,31 @@
-(ns advent-of-code-2022.aoc-utils)
+(ns advent-of-code-2022.aoc-utils
+  (:require [hato.client :as hc]
+            [clj-http.client :as client]
+            )
+  (:import
+  [java.net CookieManager URI]
+  [java.time Period ZonedDateTime]))
+
+(def c (hc/build-http-client {:connect-timeout 10000
+                              :redirect-policy :always
+                              }))
+
+(def session-cookie
+  ["session" {:value "53616c7465645f5facc39a4bb9b80d0b7b142d248894ce898ab7ec672004deeff8d13214173cce30ceb22361211623c9960a5094d55d5761f8714ba55bf2788a"
+              :domain "https://adventofcode.com"
+              }]
+  )
+
+(def my-cookie(clj-http.cookies/to-basic-client-cookie session-cookie ))
+
+(def cs
+  (clj-http.cookies/cookie-store )
+  )
+
+(clj-http.cookies/add-cookie cs my-cookie)
+
+(def input-url
+  "https://adventofcode.com/2022/day/3/input"
+  )
+
+
--- src/advent_of_code_2022/core.clj
+++ src/advent_of_code_2022/core.clj
@@ -135,7 +135,6 @@
                     (reduce +))]
     [question1 question2]))
 
-
 (defn day4
   [file-name]
   (let [
@@ -160,8 +159,7 @@
                              set-2 (set range-2)]
 
                          (or (set/subset? set-1 set-2)
-                             (set/subset? set-2 set-1)
-                             )))
+                             (set/subset? set-2 set-1))))
 
         find-overlaps (fn [ranges]
                         (let [range-1 (range (-> ranges
@@ -200,3 +198,131 @@
                     (filter #(not= 0 (count %)))
                     count)]
     [question1 question2]))
+
+
+(defn transpose [m]
+  (apply mapv vector m))
+
+(def stacks (atom {}))
+
+(defn push-pop
+  [to from]
+  (let [from-value (last (get @stacks from))
+        new-from (-> @stacks
+                     (get from)
+                     reverse
+                     rest
+                     reverse
+                     )
+        new-to (-> @stacks
+                   (get to)
+                   (concat [from-value])
+                   )
+        ]
+    (swap! stacks assoc to new-to)
+    (swap! stacks assoc from new-from)
+
+    )
+  )
+
+(defn move-something [move]
+  (let [
+        [_ antal from to] (re-matches #"move (\d+) from (\d+) to (\d+)" move)
+        from (Integer/parseInt from)
+        to (Integer/parseInt to)]
+    (println antal from to)
+    (dotimes [_ (Integer/parseInt antal)]
+      (push-pop to from)
+      )))
+
+(defn move-something-more
+  [move]
+  (let [
+        [_ antal from to] (re-matches #"move (\d+) from (\d+) to (\d+)" move)
+        from (Integer/parseInt from)
+        to (Integer/parseInt to)
+        antal (Integer/parseInt antal)
+
+        from-elements (take-last antal (get @stacks from))
+        new-to (-> @stacks
+                   (get to)
+                   (concat from-elements)
+                   )
+        new-from (drop-last antal (get @stacks from))
+        ]
+    (swap! stacks assoc to new-to)
+    (swap! stacks assoc from new-from)
+
+    )
+  )
+
+(defn day5 [filename]
+  (let [
+        input (->>
+                filename
+                slurp
+                str/split-lines
+                )
+        blank-line (.indexOf input "")
+
+        starting-positions-debug {1 [\Z \N]
+                                  2 [\M \C \D]
+                                  3 [\P]
+                                  }
+        starting-positions-matrix (->>
+                                    input
+                                    (split-at blank-line)
+                                    first
+                                    (map (fn [positions]
+                                           (->>
+                                             positions
+                                             (partition 4 4 [\s])
+                                             (map second)
+                                             )
+                                           ))
+                                    (map #(take 9 (concat % (repeat \space))))
+                                    reverse
+                                    transpose
+                                    (map #(filter (fn [z] (not= z \space)) %))
+                                    )
+
+        my-keys (map #(->
+                        %
+                        first
+                        str
+                        Integer/parseInt
+                        ) starting-positions-matrix
+                     )
+        my-vals (map rest starting-positions-matrix)
+
+        starting-positions (zipmap my-keys my-vals)
+
+        _ (reset! stacks starting-positions)
+
+        moves (->
+                input
+                (subvec (inc blank-line))
+                )
+        ]
+    (println "moves " moves)
+    (dotimes
+        [n (count moves)]
+
+        (move-something-more (nth moves n))
+      )
+
+    #_(map move-something moves)
+    #_(map move-something-more moves)
+    )
+
+  (->> (range 1 10)
+       (map #(get @stacks %))
+       (map last)
+       (apply str)
+       )
+  )
+
+(defn main
+  []
+  (day5 "resources/day5.data")
+  )
