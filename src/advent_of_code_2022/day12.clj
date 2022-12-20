1304bec5119dbc28a218e803d2bdf3b997260480
Magnus Dreyer <magnus.dreyer@gmail.com>
2022-12-20 12:58:41 +0100

    Day 12 part two done

--- src/advent_of_code_2022/day12.clj
+++ src/advent_of_code_2022/day12.clj
@@ -11,7 +11,7 @@
 (def start (atom []))
 (def end (atom []))
 (def size (atom []))
-(def solutions (atom []))
+(def list-of-as (atom []))
 
 
 (defn get-height [node]
@@ -54,56 +54,6 @@
              )
           (map #(add-nodes % node) [[1 0] [-1 0] [0 1] [0 -1]])))
 
-(defn find-a-way [node road-to-node]
-  (let [height (get-height node)
-        neighbors (get-neighbors node)
-        neighbors (filter #(contains? [-1 0 1] (- (get-height %) height)) neighbors)
-        neighbors (filter #(not (lazy-contains? road-to-node %)) neighbors)]
-    (if (empty? neighbors)
-      (do
-        []
-        )
-
-      (if (lazy-contains? neighbors @end)
-        (do
-          (println "done" (conj road-to-node @end))
-          (swap! solutions conj (conj road-to-node @end))
-          (conj road-to-node @end)
-          )
-        (doseq [neighbor neighbors]
-          (find-a-way neighbor (conj road-to-node neighbor))
-          )
-        ))
-    ))
-
-(defn day12 []
-  (let [input (->>
-                "resources/day12.data"
-                slurp
-                str/split-lines)
-        rows (count input)
-        columns (count (first input))
-        ]
-    (reset! our-graph input)
-    (reset! size [rows columns])
-    (dotimes [x rows]
-      (let [row (-> input
-                    (nth x)
-                    )
-            S (.indexOf row "S")
-            E (.indexOf row "E")
-            ]
-        (if (not= -1 S)
-          (reset! start [x S])
-          ) test
-        (if (not= -1 E)
-          (reset! end [x E])
-          )
-        )
-      )
-    )
-  )
-
 (defn stringed [node]
   (str (first node) ";" (second node))
   )
@@ -124,25 +74,49 @@
         height (get-height node)
         neighbors (get-neighbors node)
 
-        ;(filter #(contains? [0 1 -1] (- (get-height %) height)) neighbors)
-        neighbors(filter #(<  (- (get-height %) height) 2) neighbors)
+        neighbors (filter #(< (- (get-height %) height) 2) neighbors)
 
         neighbors (filter #(not (get @visited (stringed %))) neighbors)
         ]
-
-    (println (map get-height neighbors))
 
     (swap! a-graph dissoc (stringed node))
     (swap! visited assoc (stringed node) distance)
 
     (doseq [neighbor neighbors]
-      (swap! a-graph assoc (stringed neighbor) (inc distance ))
+      (swap! a-graph assoc (stringed neighbor) (inc distance))
       )
     (swap! our-graph assoc-in node \.)
     )
-
   )
 
+(defn visit-a-node-part2
+  []
+  (let [[node distance] (->>
+                          @a-graph
+                          (sort-by second)
+                          first
+                          )
+        node (de-stringed node)
+
+        height (get-height node)
+        neighbors (get-neighbors node)
+
+        neighbors (filter #(< (- height (get-height %)) 2)
+                          neighbors
+                          )
+
+        neighbors (filter #(not (get @visited (stringed %))) neighbors)
+        ]
+
+    (swap! a-graph dissoc (stringed node))
+    (swap! visited assoc (stringed node) distance)
+
+    (doseq [neighbor neighbors]
+      (swap! a-graph assoc (stringed neighbor) (inc distance))
+      )
+    (swap! our-graph assoc-in node \.)
+    )
+  )
 
 (defn d12
   []
@@ -171,24 +145,38 @@
         )
       (dotimes [y columns]
         (swap! a-graph assoc (str x ";" y) ##Inf)
+        (if (= (get-height [x y]) 97 )
+          (swap! list-of-as conj [x y])
+          )
         )
       )
-    (swap! a-graph assoc (str (first @start) ";" (second @start)) 0)
+    (swap! a-graph assoc (str (first @end) ";" (second @end)) 0)
     )
   )
 
 (defn main-part-1 []
-  (while (> (count (filter #(<  (val %) 10000) @a-graph)) 0)
+  (while (> (count (filter #(< (val %) 10000) @a-graph)) 0)
     (println "Theres is " (count @a-graph) "nodes still waiting to be looked at")
-    (println "KNown nodes on @a-graph " (count (filter #(pos-int? (val %)) @a-graph)) )
+    (println "KNown nodes on @a-graph " (count (filter #(pos-int? (val %)) @a-graph)))
     (visit-a-node)
     )
   (get @visited (stringed @end))
-)
+  )
+
+(defn main-part-2 []
+  (while (> (count (filter #(< (val %) 10000) @a-graph)) 0)
+    (visit-a-node-part2)
+    (println (map #(apply str %) @our-graph))
+    )
+  (get @visited (stringed @end))
+
+  (sort > (filter #(not (nil? %)) (map #(get @visited (stringed %))  @list-of-as)))
+  )
+
 
 (defn o [] (dotimes [_ 100]
              (
-               visit-a-node)
+               visit-a-node-part2)
              )
   (map #(apply str %) @our-graph)
   )
